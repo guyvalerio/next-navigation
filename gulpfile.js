f041abfa0bd1b77728ac6142d7b63e9db336696e
Darren Hurley <ironsidevsquincy@googlemail.com>
2015-05-13 15:24:00 +0100

    compile templates pre commit

--- gulpfile.js
+++ gulpfile.js
@@ -2,9 +2,29 @@
 
 var gulp = require('gulp');
 var jsonlint = require('gulp-jsonlint');
+var data = require('gulp-data');
+var handlebars = require('gulp-compile-handlebars');
+var rename = require("gulp-rename");
 
 gulp.task('test', function () {
 	return gulp.src('./config/*.json')
 		.pipe(jsonlint())
 		.pipe(jsonlint.reporter());
+});
+
+gulp.task('build', function () {
+	return gulp.src('./templates/*-template.html')
+		.pipe(data(function () {
+			return {
+				all: require('./config/nav.json'),
+				nav: require('./config/nav.json').filter(function (navItem) {
+					return ['world', 'companies', 'markets', 'comment', 'management', 'life & arts'].indexOf(navItem.name.toLowerCase()) > -1;
+				})
+			};
+		}))
+		.pipe(handlebars())
+		.pipe(rename(function (path) {
+			path.basename = path.basename.replace('-template', '');
+		}))
+		.pipe(gulp.dest('templates'));
 });
